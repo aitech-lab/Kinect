76f1704a98a1a3479ad76019378d57f3c159866a
peko <peko@gasubasu.com>
2012-05-10 16:23:30 +0400

    first broken proto

---
+++ of-Kinect/src/main.cpp
@@ -0,0 +1,20 @@
+#include <windows.h>
+#include <WinBase.h>
+#include <ole2.h>
+
+#include "ofMain.h"
+#include "testApp.h"
+#include "ofAppGlutWindow.h"
+
+//========================================================================
+int main( ){
+
+    ofAppGlutWindow window;
+	ofSetupOpenGL(&window, 1024,768, OF_WINDOW);			// <-------- setup the GL context
+
+	// this kicks off the running of my app
+	// can be OF_WINDOW or OF_FULLSCREEN
+	// pass in width and height too:
+	ofRunApp( new testApp());
+
+}
---
+++ of-Kinect/src/testApp.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "ofMain.h"
+#include "Kinect.h"
+
+
+#define SZ_APPDLG_WINDOW_CLASS          _T("SkeletalViewerAppDlgWndClass")
+#define WM_USER_UPDATE_FPS              WM_USER
+#define WM_USER_UPDATE_COMBO            WM_USER+1
+#define WM_USER_UPDATE_TRACKING_COMBO   WM_USER+2
+
+class testApp : public ofBaseApp{
+
+	public:
+
+		void setup ();
+		void exit  ();
+		void update();
+		void draw  ();
+
+		void keyPressed   (int key);
+		void keyReleased  (int key);
+		void mouseMoved   (int x, int y );
+		void mouseDragged (int x, int y, int button);
+		void mousePressed (int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void windowResized(int w, int h);
+		void dragEvent(ofDragInfo dragInfo);
+		void gotMessage(ofMessage msg);
+
+		Kinect kinect;
+
+};
